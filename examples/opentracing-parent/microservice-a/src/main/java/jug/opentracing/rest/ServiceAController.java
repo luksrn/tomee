e1a4dd437fd766c5424f21c50bb7290b5ca1071e
Lucas Farias <luksrn@gmail.com>
2018-12-10 21:21:06 -0200

    TOMEE-2351: Getting started with one service called 'MicroserviceA'

---
+++ examples/opentracing-parent/microservice-a/src/main/java/jug/opentracing/rest/ServiceAController.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jug.opentracing.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/serviceA")
+@ApplicationScoped
+public class ServiceAController {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(ServiceAController.class);
+
+    @GET
+    @Path("/actionA")
+    @Produces({"text/xml", "application/json"})
+    public String actionA(){
+        LOGGER.info("Log at " + ServiceAController.class.getName());
+        return "Action A";
+    }
+}
