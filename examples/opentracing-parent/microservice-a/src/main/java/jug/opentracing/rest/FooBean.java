9459a3405a98f29d9664e4d98e90060068cae3b8
Lucas Farias <luksrn@gmail.com>
2018-12-16 14:42:17 -0200

    TOMEE-2351: Client Tracing Registrar to receive notifications about cs events and correlate logs

--- examples/opentracing-parent/microservice-a/src/main/java/jug/opentracing/rest/FooBean.java
+++ examples/opentracing-parent/microservice-a/src/main/java/jug/opentracing/rest/FooBean.java
@@ -1,22 +1,20 @@
 package jug.opentracing.rest;
 
+import org.eclipse.microprofile.opentracing.ClientTracingRegistrar;
 import org.eclipse.microprofile.opentracing.Traced;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.core.Response;
 
 public class FooBean {
 
-    private Client client = ClientBuilder.newBuilder()
-            .build();
-
     @Traced
     public String callBarService(){
-        Response response = client.target("http://localhost:8081/service-b/serviceB/actionB")//"http://localhost:8080/microservice_b_war/serviceB/actionB")
-                .request()
-                .get();
 
-        return response.readEntity(String.class);
+        Client client = ClientTracingRegistrar.configure(ClientBuilder.newBuilder()).build();
+
+        return client.target("http://localhost:8081/service-b/serviceB/actionB")
+                .request()
+                .get(String.class);
     }
 }
